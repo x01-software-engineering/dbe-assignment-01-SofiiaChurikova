76cee7f94c4291807380ca8f89fecdf1e2461f96
SofiiaChurikova <150338552+SofiiaChurikova@users.noreply.github.com>
2024-03-08 04:06:54 +0200

    Create clone table
    Create clone table and add indexes

--- FBdata.sql
+++ FBdata.sql
@@ -371,4 +371,14 @@ JOIN players ON goals.player = players.id
 JOIN teams ON players.team = teams.id;
 
 
+CREATE TABLE clone_players LIKE players;
+INSERT INTO clone_players SELECT * FROM players;
+CREATE INDEX idx_nationality ON clone_players (nationality);
+
+SELECT * FROM players
+WHERE nationality = "Colombian";
+
+SELECT * FROM clone_players
+WHERE nationality = "Colombian";
+
 
