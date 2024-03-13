7d613c60cfc66c6db93921cc9b220805c1cfa04a
SofiiaChurikova <150338552+SofiiaChurikova@users.noreply.github.com>
2024-03-13 23:28:27 +0200

    Update many_to_many_query.sql

--- many_to_many_query.sql
+++ many_to_many_query.sql
@@ -12,3 +12,14 @@ JOIN team_player ON teams.id = team_player.team_id
 JOIN players ON team_player.player_id = players.id
 JOIN matches ON teams.id = matches.home_id OR teams.id = matches.away_id
 WHERE matches.id = 2;
+
+SELECT players.name AS name, teams.name AS team, matches.name AS match_name, seasons.name AS season, leagues.name AS league, COUNT(goals.id) AS total_goals
+FROM players
+JOIN goals ON players.id = goals.player
+JOIN matches ON goals.game = matches.id
+JOIN teams ON goals.team = teams.id
+JOIN match_day ON matches.match_day = match_day.id
+JOIN seasons ON match_day.season_id = seasons.id
+JOIN leagues ON match_day.league_id = leagues.id
+WHERE players.name = 'Virgil van Dijk'
+GROUP BY players.name, teams.name, matches.name, seasons.name, leagues.name;
