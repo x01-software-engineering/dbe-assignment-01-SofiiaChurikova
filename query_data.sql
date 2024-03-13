f0ea27c142d92545e5402aabd241c7cfc2c7457d
SofiiaChurikova <150338552+SofiiaChurikova@users.noreply.github.com>
2024-03-13 22:44:38 +0200

    Update and rename FBdata.sql to query_data.sql

---
+++ query_data.sql
@@ -0,0 +1,47 @@
+SELECT teams.name AS team, ROUND(AVG(YEAR(CURRENT_DATE) - YEAR((players.birthday))), 1) AS avg_age
+FROM players
+JOIN teams ON players.team = teams.id
+JOIN goals ON players.id = goals.player
+WHERE YEAR(CURRENT_DATE) - YEAR(players.birthday) < 29
+GROUP BY teams.name;
+
+SELECT players.name AS player, teams.name AS team, matches.id AS match_name, goals.goal_time AS goal
+FROM players
+JOIN goals ON players.id = goals.player
+JOIN teams ON goals.team = teams.id
+JOIN matches ON goals.game = matches.id
+ORDER BY goal;
+
+SELECT name, birthday, number
+FROM players
+WHERE number > 4
+ORDER BY name DESC
+LIMIT 15;
+
+SELECT teams.name, SUM(standings.goals_for) AS total_goals, SUM(standings.goals_against) AS miss_goals, SUM(standings.goals_for) - SUM(standings.goals_against) AS goals_difference
+FROM teams
+JOIN standings ON teams.id = standings.id
+GROUP BY teams.name
+HAVING total_goals > 1
+ORDER BY total_goals DESC;
+
+SELECT teams.name, teams.city, teams.manager_name, SUM(standings.won) AS win
+FROM teams
+JOIN standings ON teams.id = standings.id
+GROUP BY teams.name, teams.city, teams.manager_name
+ORDER BY win DESC;
+
+SELECT players.name AS name, teams.name AS team, COUNT(goals.id) AS goals
+FROM players
+JOIN team_player ON players.id = team_player.player_id
+JOIN teams ON team_player.team_id = teams.id
+LEFT JOIN goals ON players.id = goals.player
+GROUP BY players.id, teams.id
+ORDER BY team;
+
+SELECT teams.name AS team_name, players.name AS player_name
+FROM teams
+JOIN team_player ON teams.id = team_player.team_id
+JOIN players ON team_player.player_id = players.id
+JOIN matches ON teams.id = matches.home_id OR teams.id = matches.away_id
+WHERE matches.id = 2;
