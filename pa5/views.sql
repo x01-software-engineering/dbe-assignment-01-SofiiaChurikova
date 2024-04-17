d91c3da75a3ba3601d9eac83080d54e871122fee
SofiiaChurikova <150338552+SofiiaChurikova@users.noreply.github.com>
2024-04-17 12:49:53 +0300

    Remake the view
    Remake the view that if one person is in some teams, all teams will show in one row

--- pa5/views.sql
+++ pa5/views.sql
@@ -1,13 +1,14 @@
 -- Create a view that stores info about the player, team, the num of scored goals, days and time of scored goals
 CREATE VIEW best_strikers AS
-SELECT p.name AS player_name, t.name AS team_name, COUNT(1) AS total_goals, 
+SELECT p.name AS player_name, GROUP_CONCAT(DISTINCT t.name ORDER BY t.name) AS team_names, 
+COUNT(g.goal_time) AS total_goals, 
 GROUP_CONCAT(CONCAT(md.day_number, '(', g.goal_time, ')')) AS match_days_with_goals
 FROM goals g
 INNER JOIN players p ON g.player = p.id
 INNER JOIN teams t ON g.team = t.id
 INNER JOIN match_day md ON g.game IN (SELECT id FROM matches WHERE match_day = md.day_number)
-GROUP BY p.id, t.id
-ORDER BY player_name, team_name;
+GROUP BY p.name
+ORDER BY player_name;
 
 SELECT * FROM best_strikers;
 
